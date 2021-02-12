83175f727d4587fae1ac128abdd6cae344eca707
Joshua Sinyor <>
2021-02-12 18:15:10 +0000

    Update index.html, jquery.js, thermostat.js and stylesheet.css. Reflects error correction.

--- src/jquery.js
+++ src/jquery.js
@@ -1,38 +1,47 @@
 $(document).ready(function() {
   let thermostat = new Thermostat();
+
   $('#temperature-display').text(thermostat.temperature);
 
   $('#temperature-up').on('click', function() {
     thermostat._up();
-    $('#temperature-display').text(thermostat.temperature);
-    if (thermostat.max_temperature === true) {
-      $('#celsius-max-display').css("color","black");
-    } else {
-      $('#celsius-max-display').css("color","#808A87");
-    };
+    displayRefresh();
   });
 
   $('#temperature-down').on('click', function() {
     thermostat._down();
-    $('#temperature-display').text(thermostat.temperature);
-    if (thermostat.min_temperature === true) {
-      $('#celsius-min-display').css("color","black");
-    } else {
-      $('#celsius-min-display').css("color","#808A87");
-    };
+    displayRefresh();
   });
 
   $('#temperature-reset').on('click', function() {
     thermostat._reset();
-    $('#temperature-display').text(thermostat.temperature);
+    displayRefresh();
   });
 
   $('#powersavingmode').on('click', function() {
     thermostat._changePowerSavingMode();
+    displayRefresh();
+  });
+
+  function displayRefresh() {
+    $('#temperature-display').text(thermostat.temperature);
+
+    if (thermostat.minOrMaxTemperature == "min") {
+      $('#celsius-min-display').css("color", "black");
+      $('#celsius-max-display').css("color","#808A87")}
+    else if (thermostat.minOrMaxTemperature == "max") {
+      $('#celsius-min-display').css("color", "#808A87");
+      $('#celsius-max-display').css("color","black")}
+    else {
+      $('#celsius-min-display').css("color","#808A87");
+      $('#celsius-max-display').css("color","#808A87")
+    };
+
     if (thermostat.powerSavingMode === true) {
-      $('#powersavingmode-display').css("color","black");
-    } else {
+      $('#powersavingmode-display').css("color","black")}
+    else {
       $('#powersavingmode-display').css("color","#808A87");
     };
-  });
-});
+  };
+
+})
--- src/thermostat.js
+++ src/thermostat.js
@@ -6,44 +6,57 @@ class Thermostat{
     this.MINIMUM_TEMPERATURE = 10;
     this.MAXIMUM_TEMPERATURE_POWER_SAVING_MODE = 25;
     this.MAXIMUM_TEMPERATURE = 32;
-    this.MAX_TEMPERATURE = false
-    this.MIN_TEMPERATURE = false
+    this.minOrMaxTemperature = false
   };
 
   _up() {
-    if (this.MIN_TEMPERATURE = true) {this.MIN_TEMPERATURE = false};
     if (this.powerSavingMode === true && this.temperature === this.MAXIMUM_TEMPERATURE_POWER_SAVING_MODE)
-      {this.MAX_TEMPERATURE = true; return;}
+      {return}
     else if (this.powerSavingMode === false && this.temperature === this.MAXIMUM_TEMPERATURE)
-      {this.MAX_TEMPERATURE = true; return;};
-    this.temperature ++;
+      {return}
+    else
+      {this.temperature ++;
+       this._minOrMaxTemperature()};
   };
 
   _down() {
-    if (this.MAX_TEMPERATURE = true) {this.MAX_TEMPERATURE = false};
     if (this.temperature === this.MINIMUM_TEMPERATURE)
-      {this.MIN_TEMPERATURE = true; return;}
-    else {this.temperature --;};
+      {return}
+    else
+      {this.temperature --;
+       this._minOrMaxTemperature()};
   };
 
   _changePowerSavingMode() {
     this.powerSavingMode = !this.powerSavingMode;
-    if (this.powerSavingMode === true && this.temperature > this.MAXIMUM_TEMPERATURE_POWER_SAVING_MODE )
-     {this.temperature = this.MAXIMUM_TEMPERATURE_POWER_SAVING_MODE};
+    if (this.powerSavingMode === true && this.temperature > this.MAXIMUM_TEMPERATURE_POWER_SAVING_MODE)
+      {this.temperature = this.MAXIMUM_TEMPERATURE_POWER_SAVING_MODE;
+       this._minOrMaxTemperature()};
   };
 
   _reset() {
     this.temperature = 20;
+    this._minOrMaxTemperature();
+  };
+
+  _minOrMaxTemperature() {
+    if (this.temperature === this.MINIMUM_TEMPERATURE)
+      {this.minOrMaxTemperature = "min"}
+    else if (this.powerSavingMode === true && this.temperature === this.MAXIMUM_TEMPERATURE_POWER_SAVING_MODE)
+      {this.minOrMaxTemperature = "max"}
+    else if (this.powerSavingMode === false && this.temperature === this.MAXIMUM_TEMPERATURE)
+      {this.minOrMaxTemperature = "max"}
+    else
+      {this.minOrMaxTemperature = false};
   };
 
   _currentEnergyUsage() {
     if (this.temperature < 18) {
-      return "low-usage"
-    } else if (this.temperature <= 25) {
-      return "medium-usage"
-    } else {
-      return "high-usage"
-    }
+      return "low"}
+    else if (this.temperature <= 25) {
+      return "medium"}
+    else {
+      return "high"};
   };
 
 }
