cead06959021343c5b1a2a1eb62d2dd65e5d6c87
Joshua Sinyor <>
2021-02-12 16:11:24 +0000

    Update jquery.js. Reflects changing power saving mode status.

--- src/jquery.js
+++ src/jquery.js
@@ -7,14 +7,22 @@ $(document).ready(function() {
     $('#temperature-display').text(thermostat.temperature);
   });
 
-  $('#temperature-down').on('click', function(){
+  $('#temperature-down').on('click', function() {
     thermostat._down();
     $('#temperature-display').text(thermostat.temperature);
   });
 
-  $('#temperature-reset').on('click', function(){
+  $('#temperature-reset').on('click', function() {
     thermostat._reset();
     $('#temperature-display').text(thermostat.temperature);
   });
 
+  $('#powersavingmode').on('click', function() {
+    thermostat._changePowerSavingMode();
+    if (thermostat.powerSavingMode === true) {
+      $('#powersavingmode-display').css("color","black");
+    } else {
+      $('#powersavingmode-display').css("color","#808A87");
+    }
+  });
 });
