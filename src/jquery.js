81400130b35ce3f8933f81e54c10e8737944bbf0
Joshua Sinyor <>
2021-02-16 19:56:18 +0000

    Update index.html and stylesheet.css. Reflects refactoring.

--- src/jquery.js
+++ src/jquery.js
@@ -3,22 +3,22 @@ $(document).ready(function() {
 
   displayRefresh();
 
-  $('#temperature-up').on('click', function() {
+  $('#button-up').on('click', function() {
     thermostat._up();
     displayRefresh();
   });
 
-  $('#temperature-down').on('click', function() {
+  $('#button-down').on('click', function() {
     thermostat._down();
     displayRefresh();
   });
 
-  $('#temperature-reset').on('click', function() {
+  $('#button-reset').on('click', function() {
     thermostat._reset();
     displayRefresh();
   });
 
-  $('#powersavingmode').on('click', function() {
+  $('#button-powersavingmode').on('click', function() {
     thermostat._changePowerSavingMode();
     displayRefresh();
   });
@@ -26,31 +26,25 @@ $(document).ready(function() {
   function displayRefresh() {
     $('#temperature-display').text(thermostat.temperature);
 
-    $('#currentenergyusage-low-display').css("color","#808A87");
-    $('#currentenergyusage-medium-display').css("color", "#808A87");
-    $('#currentenergyusage-high-display').css("color","#808A87");
-    $('#celsius-min-display').css("color","#808A87");
-    $('#celsius-max-display').css("color","#808A87");
-    $('#powersavingmode-display').css("color","#808A87");
+    $('.refresh').css("color","#808A87");
 
     if (thermostat.currentEnergyUsage === "low") {
-      $('#currentenergyusage-low-display').css("color","black")
+      $('#currentenergyusage-low').css("color","black")
     } else if (thermostat.currentEnergyUsage === "medium") {
-      $('#currentenergyusage-medium-display').css("color", "black")
+      $('#currentenergyusage-medium').css("color", "black")
     } else {
-      $('#currentenergyusage-high-display').css("color","black")
+      $('#currentenergyusage-high').css("color","black")
     };
 
     if (thermostat.minOrMaxTemperature === "min") {
-      $('#celsius-min-display').css("color", "black")
+      $('#celsius-min').css("color", "black")
     } else if (thermostat.minOrMaxTemperature === "max") {
-      $('#celsius-max-display').css("color", "black")
+      $('#celsius-max').css("color", "black")
     };
 
     if (thermostat.powerSavingMode === true) {
-      $('#powersavingmode-display').css("color","black")
+      $('#powersavingmode').css("color","black")
     };
-
   };
 
 })
